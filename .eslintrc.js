9b31feb0da324fb3b868c776ee8f5dfa8683add9
Carlo M <carlo992@gmail.com>
2022-03-24 16:15:50 +0100

    Migrated package and eslint config to support prettier plugin with prettier config fetched from boolean's npm package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,14 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "standard"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 12
-    },
-    "rules": {
-    }
-};
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jasmine: true
+  },
+  extends: ['standard', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: { 'prettier/prettier': ['error'] }
+}
